309178700f219f4bfde7808372c4789dd5dd6a0e
KingsDevs <sunnyckair@gmail.com>
2022-07-02 14:38:37 +0800

    added weekend subscriber class

---
+++ src/WeekendSubsciber.java
@@ -0,0 +1,20 @@
+
+public class WeekendSubsciber extends NewsPaperSubscriber
+{
+    @Override
+    public void setSubscriptionRate() {
+        super.subscriptionRate = 3.0;
+    }
+
+    public WeekendSubsciber(String streetAdd)
+    {
+        setSubscriptionRate();
+        super.setStreetAdd(streetAdd);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Weekend Subscriber | Rate: " + super.getSubscriptionRate() + " | Street Address: " + super.getStreetAdd();
+    }
+}
